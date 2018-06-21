2a1d7e4a90ef15f22e0edbbb5551a60a413a4433
AdnoC <adam.r.cutler@gmail.com>
2018-06-20 20:11:20 -0400

    Some work on Dp and DpVoid

--- src/deferred_heap.rs
+++ src/deferred_heap.rs
@@ -1,14 +1,15 @@
 use page_size;
 use std::marker::PhantomData;
-use std::cell::RefCell;
+// use std::cell::RefCell;
 
 use chunk::Chunk;
+use bit_vec::BitVec;
 
 // Storing/getting drop impl: https://doc.rust-lang.org/1.1.0/src/arena/lib.rs.html#187
 
 // longer_than_self lifetime from any_arena crate
 pub struct DeferredHeap<'longer_than_self> {
-    chunks: RefCell<ChunkList>,
+    // pages: Vec<DhPage>,
     _marker: PhantomData<*mut &'longer_than_self ()>,
 }
 
@@ -19,7 +20,7 @@ impl<'a> DeferredHeap<'a> {
 
     pub fn with_size(chunk_size: usize) -> DeferredHeap<'a> {
         DeferredHeap {
-            chunks: RefCell::new(ChunkList::with_size(chunk_size)),
+            // chunks: RefCell::new(ChunkList::with_size(chunk_size)),
             _marker: PhantomData,
         }
     }
@@ -39,6 +40,164 @@ impl ChunkList {
     }
 }
 
+// TODO
 pub struct Dp<T> {
     phantom: PhantomData<T>,
+}
+
+// struct DpVoid<'a, 'b> {
+//     heap: &'a DeferredHeap<'b>,
+//     ptr: *const i8,
+// }
+
+struct NonRoot {
+    // ptr: *const DpVoid,
+    level: usize,
+}
+
+struct DhPage<'a> {
+    page: Chunk,
+    live_starts: BitVec,
+    deferred_ptrs: Vec<NonRoot>,
+    heap: *mut DeferredHeap<'a>,
+}
+
+#[cfg(target_os = "ios")]
+mod use_tests {
+    use super::*;
+
+    fn graph_stuff() {
+        // This function has implicit `Some`s
+        struct LLNode<T> {
+            data: T,
+            prev: Option<Dp<RefCell<LLNode<T>>>>,
+            next: Option<Dp<RefCell<LLNode<T>>>>,
+        }
+
+        let dheap = DeferredHeap::new();
+
+        let a: Dp<LLnode<_>> = dheap.insert(LLNode::new(1));
+        // rt: a        nr:
+        let b = dheap.insert(LLNode::new(2));
+        // rt: a, b     nr:
+        {
+            let c = dheap.insert(LLNode::new(3));
+            // rt: a, b, c  nr:
+
+            a.borrow_mut().prev = c.clone();
+            a.borrow_mut().next = b.clone();
+
+            b.borrow_mut().prev = a.clone();
+            b.borrow_mut().next = c.clone();
+
+            c.borrow_mut().prev = b.clone();
+            c.borrow_mut().next = a.clone();
+
+        }
+        // rt: a, b     nr: c
+
+        let mut ll = LLNode::new(4);
+        ll.next = b.clone();
+        // *b.next.borrow_mut() = ll; // c = ll, which has root `Dp`s
+
+        {
+            let d: Dp<RefCell<LLNode<_>>> = dheap.insert(RefCell::new(LLNode::new(1)));
+            *d.borrow_mut().next.borrow_mut() = ll.clone();
+            *d.borrow_mut().next.borrow_mut() = b.clone();
+            *d.borrow_mut().next.borrow_mut() = a.prev.clone();
+        }
+
+        *b = a.clone(); // Only if DerefMut
+        // b.assign(a.clone()); // If only Deref
+        b = a.clone();
+        // rt: a        nr: b, c
+
+        a.borrow_mut().next.borrow_mut().next = None; // b.next = None
+    }
+
+    fn graph_stuff2() {
+        // This function has implicit `Some`s
+        struct LLNode<T> {
+            data: T,
+            prev: Option<Dp<RefCell<LLNode<T>>>>,
+            next: Option<Dp<RefCell<LLNode<T>>>>,
+        }
+
+        let dheap = DeferredHeap::new();
+
+        let a: Dp<LLnode<_>> = dheap.insert(LLNode::new(1));
+        // rt: a        nr:
+        let b = dheap.insert(LLNode::new(2));
+        // rt: a, b     nr:
+        {
+            let c = dheap.insert(LLNode::new(3));
+            // rt: a, b, c  nr:
+
+            a.borrow_mut().prev.assign(&mut c);
+            a.borrow_mut().next.assign(&mut b);
+
+            b.borrow_mut().prev.assign(&mut a);
+            b.borrow_mut().next.assign(&mut c);
+
+            c.borrow_mut().prev.assign(&mut b);
+            c.borrow_mut().next.assign(&mut a);
+
+        }
+        // rt: a, b     nr: c
+
+        let mut ll = LLNode::new(4);
+        ll.next = b.clone();
+        // *b.next.borrow_mut() = ll; // c = ll, which has root `Dp`s
+
+        {
+            let d: Dp<RefCell<LLNode<_>>> = dheap.insert(RefCell::new(LLNode::new(1)));
+            let e = dheap.insert(LLNode::new(0));
+            let ll2 = LLNode::new(0);
+            // *d.borrow_mut().next.borrow_mut() = e; // Doesn't compile due to only Deref
+            // *d.borrow_mut().next.borrow_mut() = ll2; // Doesn't compile due to only Deref
+            *d.borrow_mut().next.borrow_mut().assign(&mut a.prev);
+        }
+
+        *b = a.clone(); // Only if DerefMut
+        b.assign(&mut a); // If only Deref
+        // b = a.clone();
+        // rt: a        nr: b, c
+
+        a.borrow_mut().next.borrow_mut().next = None; // b.next = None
+    }
+
+    impl<T> Deref<Target=T> for Dp<T> {}
+    #[derive(Clone)]
+    struct Dp<T: Traceable> {}
+    impl DeferredHeap {
+        fn insert<T: Traceable>(&mut self, data: T) {
+            let place = self.alloc::<T>();
+            *place = data;
+            place.set_root();
+        }
+    }
+    impl<T: Traceable> Dp<T> {
+        fn assign(&mut self, other: Dp<T>) {
+            let was_in_heap = self.is_in_heap();
+            *self = other;
+            if self.is_in_heap() && !was_in_heap {
+                self.set_non_root();
+            } else if self.is_root() && was_in_heap {
+                self.set_root();
+            }
+        }
+        fn new(data: T) -> Dp<T> {
+            let ptr = Dp { data: T };
+            ptr.set_root();
+            ptr
+        }
+    }
+    impl<T: Traceable> Drop for Dp<T> {
+        fn drop(self) {
+            if self.is_root() {
+                self.unregister();
+            }
+        }
+    }
+
 }
--- src/msc_test_mods.rs
+++ src/msc_test_mods.rs
@@ -1,3 +1,175 @@
+mod tracking_root_status {
+    use std::marker::PhantomData;
+
+    trait Traceable {}
+    #[derive(Default)]
+    struct DeferredHeap<'longer_than_self, T: Traceable> {
+        data: Vec<T>,
+        nonroots: Vec<DpVoid>,
+        _marker: PhantomData<*mut &'longer_than_self ()>,
+    }
+
+    impl<'a, T: Traceable> DeferredHeap<'a, T> {
+        fn new() -> DeferredHeap<'a, T> {
+                DeferredHeap {
+                data: Vec::with_capacity(32),
+                nonroots: vec![],
+                _marker: Default::default(),
+            }
+        }
+
+        fn insert_ref(&mut self, data: T) -> &mut T {
+            self.data.push(data);
+            let len = self.data.len();
+            &mut self.data[len - 1]
+        }
+        fn insert<'b>(&'b mut self, data: T) -> Dp<'b, T> where 'b: 'a {
+            use std::mem::transmute;
+
+            self.data.push(data);
+            let len = self.data.len();
+            Dp {
+                _marker: Default::default(),
+                impl_ptr: DpVoid {
+                    heap: unsafe {transmute(self as *const DeferredHeap<'a, T>)},
+                    ptr: unsafe {transmute(&self.data[len-1] as *const T)},
+                }
+            }
+        }
+    }
+
+    struct Dp<'a, T: Traceable> where T: 'a {
+        _marker: PhantomData<*mut &'a T>,
+        impl_ptr: DpVoid,
+    }
+    impl<'a, T: Traceable> Dp<'a, T> {
+        fn borrow_mut(&mut self) -> &mut T {
+            use std::mem::transmute;
+            let ptr: *mut T = unsafe {transmute(self.impl_ptr.ptr)};
+            unsafe {&mut *ptr}
+        }
+        fn borrow(&self) -> &T {
+            use std::mem::transmute;
+            let ptr: *const T = unsafe {transmute(self.impl_ptr.ptr)};
+            unsafe {&*ptr}
+        }
+    }
+    impl<'a, T: Traceable> ::std::ops::Deref for Dp<'a, T> where T: 'a {
+        type Target = T;
+        fn deref(&self) -> &T {
+            self.borrow()
+        }
+
+    }
+
+    struct DpVoid {
+        heap: *const DeferredHeap<'static, Payload>,
+        ptr: *const i8,
+    }
+
+    struct Payload {
+        data: i32,
+    }
+    impl Traceable for Payload {}
+    impl Payload {
+        fn new_default(data: i32) -> Payload {
+            Payload {
+                data,
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn make_new_dp() {
+            // { // Shouldn't compile (due to borrowck)
+            //     let ptr;
+            //     {
+            //         let mut heap: DeferredHeap<Payload> = DeferredHeap::new();
+            //         ptr = heap.insert(Payload { data: 3 });
+            //     }
+            //     assert_eq!(unsafe {&*ptr.ptr}.data, 3);
+            // }
+
+            let mut heap: DeferredHeap<Payload> = DeferredHeap::new();
+            let ptr = heap.insert(Payload::new_default(3));
+            assert_eq!(ptr.borrow().data, 3);
+            assert_eq!(ptr.data, 3);
+        }
+    }
+
+}
+
+mod reference_distributer_with_custom_ptr_type {
+    use std::marker::PhantomData;
+
+    trait Traceable {}
+    #[derive(Default)]
+    struct DeferredHeap<'longer_than_self, T: Traceable> {
+        data: Vec<T>,
+        _marker: PhantomData<*mut &'longer_than_self ()>,
+    }
+
+    impl<'a, T: Traceable> DeferredHeap<'a, T> {
+        fn new() -> DeferredHeap<'a, T> {
+                DeferredHeap {
+                data: Vec::with_capacity(32),
+                _marker: Default::default(),
+            }
+        }
+
+        fn insert_ref(&mut self, data: T) -> &mut T {
+            self.data.push(data);
+            let len = self.data.len();
+            &mut self.data[len - 1]
+        }
+        fn insert<'b>(&'b mut self, data: T) -> Dp<'b, T> where 'b: 'a {
+            self.data.push(data);
+            let len = self.data.len();
+            Dp {
+                _marker: Default::default(),
+                heap: self as *const _,
+                ptr: &self.data[len-1] as *const _,
+            }
+        }
+    }
+
+    struct Dp<'a, T: Traceable> where T: 'a {
+        _marker: PhantomData<*mut &'a ()>,
+        heap: *const DeferredHeap<'a, T>,
+        ptr: *const T,
+    }
+
+    struct Payload {
+        data: i32,
+    }
+    impl Traceable for Payload {}
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn make_new_dp() {
+            // { // Shouldn't compile (due to borrowck)
+            //     let ptr;
+            //     {
+            //         let mut heap: DeferredHeap<Payload> = DeferredHeap::new();
+            //         ptr = heap.insert(Payload { data: 3 });
+            //     }
+            //     assert_eq!(unsafe {&*ptr.ptr}.data, 3);
+            // }
+
+            let mut heap: DeferredHeap<Payload> = DeferredHeap::new();
+            let ptr = heap.insert(Payload { data: 3 });
+            assert_eq!(unsafe {&*ptr.ptr}.data, 3);
+        }
+    }
+}
+
 mod type_erased_dropping {
     struct Dropper {
         drop_glue: fn(*const i8),
@@ -95,6 +267,15 @@ mod reference_distributer {
                 assert_eq!(*val, 3);
             }
         }
+
+        // #[test]
+        // fn does_not_let_outlive() {
+        //     let val_ref;
+        //     {
+        //         let mut rdist = RefDist::new();
+        //         val_ref = rdist.index(0);
+        //     }
+        // }
 
     }
 }
