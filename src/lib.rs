2836a8347e712cb6cd84a2831764de503a10dd42
AdnoC <adam.r.cutler@gmail.com>
2018-06-27 22:05:19 -0400

    Rename to 'terse'

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
-#![allow(dead_code)]
-#![feature(core_intrinsics, ptr_offset_from)]
 
-extern crate itertools;
-extern crate page_size;
-extern crate bit_vec;
-
-mod destructors;
-mod msc_test_mods; // For dev purposes only
-mod chunk;
-mod deferred_heap;
-pub use deferred_heap::*;
+pub mod collector;
