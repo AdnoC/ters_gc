c59ff50c4a71686602c380e6739aa679557021ea
AdnoC <adam.r.cutler@gmail.com>
2018-06-27 23:19:06 -0400

    Removed unused test mod

--- src/arena.rs
+++ src/arena.rs
@@ -37,29 +37,3 @@ pub struct Gc<'arena, T> {
     _marker: PhantomData<*mut &'arena ()>,
     ptr: *mut T,
 }
-
-// #[cfg(test)]
-// mod test {
-//     use super::*;
-//     #[test]
-//     fn lifetimes_are_properly_constrained() {
-//         use ::std::mem::drop;
-//         let mut arena = Arena::new();
-//         let mut alloc = arena.allocator();
-//
-//         let gc_ptr = alloc.store(42);
-//
-//         drop(alloc);
-//
-//         // Should borrowck error. Can't move arena since it is borrowed by gc_ptr
-//         let arena2 = arena; //~ ERROR error[E0505]: cannot move out of `arena` because it is borrowed
-//
-//         //```compile_fail
-//         //let arena2 = arena;
-//         //```
-//
-//         //```compile_fail
-//         //let arena2 = //arena;
-//         //```
-//     }
-// }
