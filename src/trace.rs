8290f0519f70bddf02a123a0588700a2c09f7c6f
AdnoC <adam.r.cutler@gmail.com>
2018-07-11 05:48:04 -0400

    ::trace docs

--- src/trace.rs
+++ src/trace.rs
@@ -8,10 +8,117 @@
 //! references to. An incomplete [`Trace`] implementation will result in
 //! memory leaks.
 //!
-//! A correct [`Trace`] implementation calls `Tracer::add_target` on all members
+//! A correct [`Trace`] implementation calls [`Tracer::add_target`] on all members
 //! that can contain a [`Gc`].
 //!
+//! # Examples
+//!
+//! A impl for a large, complex struct:
+//!
+//! ```
+//! use ters_gc::{Collector, Proxy};
+//! use ters_gc::trace::{Trace, Tracer};
+//! use ters_gc::ptr::{Gc, Weak};
+//!
+//! use std::sync::Arc;
+//! use std::cell::RefCell;
+//! use std::time::Instant;
+//! use std::ffi::OsString;
+//!
+//!
+//! // Newtype that just prints a message whenever it is traced
+//! struct TracePrinter<T: Trace>(T);
+//! impl<T: Trace> Trace for TracePrinter<T> {
+//!     fn trace(&self, tracer: &mut Tracer) {
+//!         println!("trace occurred!");
+//!         // Forward the trace to the inner type
+//!         tracer.add_target(&self.0);
+//!     }
+//! }
+//!
+//! // Complex data structure.
+//! // Has 4 members that can contain `Gc`s
+//! struct BigComplexThing<'a> {
+//!     name: TracePrinter<Gc<'a, OsString>>,
+//!     description: String,
+//!     root_obj: Arc<RefCell<Option<TracePrinter<Gc<'a, i32>>>>>,
+//!     timestamp: TracePrinter<Gc<'a, Instant>>,
+//!     children: RefCell<Vec<TracePrinter<Gc<'a, i32>>>>,
+//!     a_number: i32,
+//! }
+//!
+//! impl<'a> Trace for BigComplexThing<'a> {
+//!     fn trace(&self, tracer: &mut Tracer) {
+//!         // Call `add_target` on all members containing `Gc`s
+//!         tracer.add_target(&self.name);
+//!         tracer.add_target(&self.root_obj);
+//!         tracer.add_target(&self.timestamp);
+//!         tracer.add_target(&self.children);
+//!     }
+//! }
+//!
+//! Collector::new().run_with_gc(|mut proxy| {
+//!     let bct_stack = BigComplexThing {
+//!         name: TracePrinter(proxy.store(OsString::new())),
+//!         description: "default description".to_string(),
+//!         root_obj: Arc::new(RefCell::new(None)),
+//!         timestamp: TracePrinter(proxy.store(Instant::now())),
+//!         children: RefCell::new(Vec::new()),
+//!         a_number: 0,
+//!     };
+//!
+//!     let mut bct = proxy.store(TracePrinter(bct_stack));
+//!
+//!     println!("Running");
+//!     proxy.run(); // Prints "trace occurred!" 3*N times
+//!                  // (where N is the number of traces per collection run)
+//!
+//!     println!("Adding more");
+//!
+//!     // 2-step process. First make the Gc, then store it.
+//!     // Otherwise we might start automatic collection when we store the `0`.
+//!     // If that happens, we'll panic because we borrowed `children`
+//!     // in order to push the entry, but the trace will also try to
+//!     // borrow it.
+//!     for _ in 0..3 {
+//!         let entry = TracePrinter(proxy.store(0));
+//!         bct.0.children.borrow_mut().push(entry);
+//!     }
+//!
+//!     println!("Running again");
+//!     proxy.run(); // Prints "trace occurred!" 6*N times
+//!
+//!     println!("Setting root");
+//!     let entry = TracePrinter(proxy.store(0));
+//!     *bct.0.root_obj.borrow_mut() = Some(entry);
+//!
+//!     println!("Running once more");
+//!     proxy.run(); // Prints "trace occurred!" 7*N times
+//! });
+//! ```
+//!
+//! [`Trace`] has a default implementation for structs that don't contain
+//! any [`Gc`] pointers
+//!
+//! ```
+//! use ters_gc::Collector;
+//! use ters_gc::trace::Trace;
+//!
+//! struct I32Newtype(i32);
+//!
+//! // Give `I32Newtype` a noop implementation
+//! impl Trace for I32Newtype {}
+//!
+//! Collector::new().run_with_gc(|mut proxy| {
+//!     proxy.store(I32Newtype(22));
+//! });
+//! ```
+//!
+//!
+//! 
 //! [`Trace`]: trait.Trace.html
+//! [`Tracer::add_target`]: struct.Tracer.html#method.add_target
+//! [`Gc`]: ../ptr/struct.Gc.html
 
 use ptr::{Gc, GcBox, Weak};
 use std::ptr::NonNull;
@@ -43,6 +150,7 @@ impl Tracer {
     pub(crate) fn new() -> Tracer {
         Tracer { targets: vec![] }
     }
+    /// Add a trace target
     pub fn add_target<T: Trace + ?Sized>(&mut self, target: &T) {
         target.trace(self);
     }
@@ -79,6 +187,7 @@ mod trace_impls {
         ($($T:ty)+) => {
             $(
                 impl Trace for $T {
+                    /// Noop
                     fn trace(&self, _: &mut Tracer) {
                         // noop
                     }
@@ -114,6 +223,7 @@ mod trace_impls {
         std::time::SystemTime
     }
     impl<'a> Trace for &'a str {
+        /// Noop
         fn trace(&self, _: &mut Tracer) {
             // noop
         }
@@ -121,6 +231,7 @@ mod trace_impls {
     macro_rules! noop_fn_impl {
         ($($T:tt)*) => {
             impl<$($T,)* R> Trace for fn($($T),*) -> R {
+                /// Noop
                 fn trace(&self, _: &mut Tracer) {
                     // noop
                 }
@@ -133,17 +244,20 @@ mod trace_impls {
     noop_fn_impl!(Q W E);
     noop_fn_impl!(Q W E T);
     impl<T: ?Sized> Trace for *const T {
+        /// Noop
         fn trace(&self, _: &mut Tracer) {
             // noop
         }
     }
     impl<T: ?Sized> Trace for *mut T {
+        /// Noop
         fn trace(&self, _: &mut Tracer) {
             // noop
         }
     }
 
     impl<'a, T: Trace> Trace for [T] {
+        /// Traces each element
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self.iter() {
                 tracer.add_target(tracee);
@@ -154,6 +268,7 @@ mod trace_impls {
         ($($N:expr)+) => {
             $(
                 impl<T: Trace> Trace for [T; $N] {
+                    /// Traces each element
                     fn trace(&self, tracer: &mut Tracer) {
                         tracer.add_target(&self[..]);
                     }
@@ -169,6 +284,7 @@ mod trace_impls {
     }
 
     impl<T: Trace> Trace for Option<T> {
+        /// Traces inner value if `Some`
         fn trace(&self, tracer: &mut Tracer) {
             if let Some(ref contents) = self {
                 tracer.add_target(contents);
@@ -177,6 +293,7 @@ mod trace_impls {
     }
     // TODO: Is this one a good idea?
     impl<T: Trace, E> Trace for Result<T, E> {
+        /// Traces inner object if `Ok`
         fn trace(&self, tracer: &mut Tracer) {
             if let Ok(ref contents) = self {
                 tracer.add_target(contents);
@@ -184,17 +301,20 @@ mod trace_impls {
         }
     }
     impl<T: Trace + ?Sized> Trace for Box<T> {
+        /// Traces inner object (via deref)
         fn trace(&self, tracer: &mut Tracer) {
             let contents: &T = &*self;
             tracer.add_target(contents);
         }
     }
     impl<'a, T: Trace + 'a + ToOwned + ?Sized> Trace for std::borrow::Cow<'a, T> {
+        /// Traces inner object (via deref)
         fn trace(&self, tracer: &mut Tracer) {
             tracer.add_target(&*self);
         }
     }
     impl<T: Trace> Trace for Vec<T> {
+        /// Traces each element
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self {
                 tracer.add_target(tracee);
@@ -202,6 +322,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace + ?Sized> Trace for std::rc::Rc<T> {
+        /// Traces inner object (via deref)
         fn trace(&self, tracer: &mut Tracer) {
             let contents: &T = &*self;
             tracer.add_target(contents);
@@ -209,6 +330,7 @@ mod trace_impls {
     }
     impl<T: Trace + ?Sized> Trace for std::rc::Weak<T> {}
     impl<T: Trace + ?Sized> Trace for std::sync::Arc<T> {
+        /// Traces inner object (via deref)
         fn trace(&self, tracer: &mut Tracer) {
             let contents: &T = &*self;
             tracer.add_target(contents);
@@ -216,11 +338,13 @@ mod trace_impls {
     }
     impl<T: Trace + ?Sized> Trace for std::sync::Weak<T> {}
     impl<T: Trace + ?Sized> Trace for std::cell::RefCell<T> {
+        /// Borrows (Via `RefCell::borrow`) self and traces inner object
         fn trace(&self, tracer: &mut Tracer) {
             tracer.add_target(&*self.borrow());
         }
     }
     impl<T: Trace> Trace for std::collections::VecDeque<T> {
+        /// Traces each element
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self {
                 tracer.add_target(tracee);
@@ -228,6 +352,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace> Trace for std::collections::LinkedList<T> {
+        /// Traces each element
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self {
                 tracer.add_target(tracee);
@@ -235,6 +360,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace, K: Eq + Hash> Trace for std::collections::HashMap<K, T> {
+        /// Traces each value
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self.values() {
                 tracer.add_target(tracee);
@@ -242,6 +368,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace, K: Eq + Hash> Trace for std::collections::BTreeMap<K, T> {
+        /// Traces each value
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self.values() {
                 tracer.add_target(tracee);
@@ -249,6 +376,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace + Eq + Hash> Trace for std::collections::HashSet<T> {
+        /// Traces each value
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self {
                 tracer.add_target(tracee);
@@ -256,6 +384,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace + Eq + Hash> Trace for std::collections::BTreeSet<T> {
+        /// Traces each value
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self {
                 tracer.add_target(tracee);
@@ -263,6 +392,7 @@ mod trace_impls {
         }
     }
     impl<T: Trace + Ord> Trace for std::collections::BinaryHeap<T> {
+        /// Traces each value
         fn trace(&self, tracer: &mut Tracer) {
             for tracee in self {
                 tracer.add_target(tracee);
